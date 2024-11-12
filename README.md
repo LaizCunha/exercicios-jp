22e9c04c2923ce0b9849500c3c22cce9d394b207
João Paulo Paschoal <jneto@conder.ba.gov.br>
2024-11-12 17:52:40 -0300

    Introduz template base e descrição do primeiro desafio do exercício.

---
+++ README.md
@@ -0,0 +1,15 @@
+## Exercícios JP
+
+### **Desafio 1: Renderizar cards dos pets.**
+
+Percorrer a lista de pets no javascript (estática por enquanto) e para cada pet:
+- Criar uma div para o card do pet;
+- Estilizar o card de acordo com a coloração do pet (criar temas no css?);
+- Usar o grid system do bootstrap para permitir responsividade da exibição dos cards em dispositivos com telas menores;
+- Usar o componente card do bootstrap.
+
+---
+
+### **Desafio 2: Aplicar filtros sobre os pets.**
+
+Criar controles e inputs para permitir filtrar os pets como, por espécie, faixa de peso, ...
---
+++ main.js
@@ -0,0 +1,37 @@
+const listaPets = [
+    {
+        nome: 'Chantilly',
+        especie: 'Gato',
+        coloracao: 'Tricolor',
+        pesoKg: 7.8,
+        Castrado: true
+    },
+    {
+        nome: 'Cheetos',
+        especie: 'Gato',
+        coloracao: 'Laranja',
+        pesoKg: 5.2,
+        Castrado: false
+    },
+    {
+        nome: 'Mel',
+        especie: 'Cachorro',
+        coloracao: 'Preto',
+        pesoKg: 13,
+        Castrado: true
+    },
+    {
+        nome: 'AL',
+        especie: 'Cachorro',
+        coloracao: 'Caramelo',
+        pesoKg: 20,
+        Castrado: true
+    },
+    {
+        nome: 'Hantaro',
+        especie: 'Hamster',
+        coloracao: 'Branco',
+        pesoKg: 0.3,
+        Castrado: false
+    }
+];
