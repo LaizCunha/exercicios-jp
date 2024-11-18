18554cdaaf04edb419cb92160ed9611e4d648388
LaizCunha <laizsc.89@gmail.com>
2024-11-18 15:33:45 -0300

    ajusta o camelcase

--- main.js
+++ main.js
@@ -5,35 +5,35 @@ $(document).ready(function() {
             especie: 'Gato',
             coloracao: 'Tricolor',
             pesoKg: 7.8,
-            Castrado: true
+            castrado: true
         },
         {
             nome: 'Cheetos',
             especie: 'Gato',
             coloracao: 'Laranja',
             pesoKg: 5.2,
-            Castrado: false
+            castrado: false
         },
         {
             nome: 'Malu',
             especie: 'Cachorro',
             coloracao: 'Preto',
             pesoKg: 13,
-            Castrado: true
+            castrado: true
         },
         {
             nome: 'AL',
             especie: 'Cachorro',
             coloracao: 'Caramelo',
             pesoKg: 20,
-            Castrado: true
+            castrado: true
         },
         {
             nome: 'Hantaro',
             especie: 'Hamster',
             coloracao: 'Branco',
             pesoKg: 0.3,
-            Castrado: false
+            castrado: false
         }
     ];
 
@@ -43,7 +43,7 @@ $(document).ready(function() {
           <img src="/imgs/${pet.nome}.jpg" class="card-img-top" alt="...">
             <div class="card-body pet-color" >
                 <h5 class="card-title">${pet.nome}</h5>
-                <p class="pet-info">Espécie: ${pet.especie}, Cor: ${pet.coloracao}, ${pet.pesoKg}kgs Castrado: ${pet.Castrado ?  'Sim' : 'Não'}</p>
+                <p class="pet-info">Espécie: ${pet.especie}, Cor: ${pet.coloracao}, ${pet.pesoKg}kgs Castrado: ${pet.castrado ?  'Sim' : 'Não'}</p>
             </div>
         </div>
         `;
