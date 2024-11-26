e0339d51d30d72211c44bd51dd9186b8b7849f54
João Paulo Paschoal <jpvitor.neto@gmail.com>
2024-11-25 23:47:52 -0300

    Inclui novos exercícios.

--- main.js
+++ main.js
@@ -1,41 +1,5 @@
 $(document).ready(function() {
-    const listaPets = [
-        {
-            nome: 'Chantilly',
-            especie: 'Gato',
-            coloracao: 'Tricolor',
-            pesoKg: 7.8,
-            castrado: true
-        },
-        {
-            nome: 'Cheetos',
-            especie: 'Gato',
-            coloracao: 'Laranja',
-            pesoKg: 5.2,
-            castrado: false
-        },
-        {
-            nome: 'Malu',
-            especie: 'Cachorro',
-            coloracao: 'Preto',
-            pesoKg: 13,
-            castrado: true
-        },
-        {
-            nome: 'AL',
-            especie: 'Cachorro',
-            coloracao: 'Caramelo',
-            pesoKg: 20,
-            castrado: true
-        },
-        {
-            nome: 'Hantaro',
-            especie: 'Hamster',
-            coloracao: 'Branco',
-            pesoKg: 0.3,
-            castrado: false
-        }
-    ];
+    const listaPets = [];
 
     listaPets.forEach(function(pet) {
         var card = `
