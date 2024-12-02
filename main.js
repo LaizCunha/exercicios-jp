0e7dba2d9bdfefddc8c3e0cc64c3360bc0960992
LaizCunha <laizsc.89@gmail.com>
2024-12-02 08:35:57 -0300

    Cria API fake e altera o código para adicionar registros de pets através de requisição.

--- main.js
+++ main.js
@@ -38,31 +38,37 @@ $(document).ready(function() {
     }
   ];
 
-  // TODO: preencher array de pets partindo de requisição para fake api
-  // const listaPets = [];
-
-  for (const pet of listaPets) {
-    var card = $("<div>").addClass("card");
-    var cardHead = $("<div>").addClass("card-head");
-    var cardBody = $("<div>").addClass("card-body");
-    var tema = pet.coloracao.toLocaleLowerCase();
-        
-    cardHead.append(`
-      <img src="/imgs/${pet.nome}.jpg" alt"Foto de ${pet.nome}">
-      <p>${pet.nome}</p>
-    `);
-    cardBody.append(`
-      <p>Espécie: ${pet.especie}</p>
-      <p>Coloração: ${pet.coloracao}</p>
-      <p>Peso: ${pet.pesoKg}kg</p>
-      <p>Castrado: ${pet.castrado ? "Sim" : "Não"}</p>
-    `)        
-        
-    card.append(cardHead);
-    card.append(cardBody);
-    card.addClass(tema);
-        
-    $(".cards").append(card);
-  }
-
+  $.ajax({
+    url: "http://localhost:3000/pets",
+    method: "GET",
+    success: function (listaPets) {
+      for (const pet of listaPets) {
+        var card = $("<div>").addClass("card");
+        var cardHead = $("<div>").addClass("card-head");
+        var cardBody = $("<div>").addClass("card-body");
+        var tema = pet.coloracao.toLocaleLowerCase();
+            
+        cardHead.append(`
+          <img src="/imgs/${pet.nome}.jpg" alt"Foto de ${pet.nome}">
+          <p>${pet.nome}</p>
+        `);
+        cardBody.append(`
+          <p>Espécie: ${pet.especie}</p>
+          <p>Coloração: ${pet.coloracao}</p>
+          <p>Peso: ${pet.pesoKg}kg</p>
+          <p>Castrado: ${pet.castrado ? "Sim" : "Não"}</p>
+        `)        
+            
+        card.append(cardHead);
+        card.append(cardBody);
+        card.addClass(tema);
+            
+        $(".cards").append(card);
+      }
+    },
+    error: function (error) {
+      console.log("Erro ao acessar a API:", error);
+    }  
+  });
+  
 });
